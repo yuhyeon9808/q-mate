ab7970a3ee6cd477bc8733c6da90122cdd241aa2
yuhyeon9808 <bwj9808@gmail.com>
2025-10-10 13:07:28 +0000

    refactor:add-swagger-setting

--- back/src/main/java/com/qmate/api/AuthExchangeController.java
+++ back/src/main/java/com/qmate/api/AuthExchangeController.java
@@ -1,9 +1,12 @@
 package com.qmate.api;
 
+import com.qmate.common.constants.auth.AuthConstants;
 import com.qmate.domain.auth.JwtService;
 import com.qmate.domain.auth.model.response.LoginResponse;
 import com.qmate.domain.user.UserRepository;
 import com.qmate.security.UserPrincipal;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,12 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "Auth Exchage", description = "쿠키를 통해 로그인 response 받는 API")
 public class AuthExchangeController {
 
   private final JwtService jwtService;
   private final UserRepository userRepository;
 
   @PostMapping("/exchange")
+  @Operation(
+      summary = "쿠키를 통해 액세스 토큰 포함 로그인 후 필요한 정보 반환",
+      description = AuthConstants.REGISTER_MD
+  )
   public ResponseEntity<LoginResponse> exchange(
       @CookieValue(name = "ACCESS_TOKEN", required = false) String accessCookie,
       @AuthenticationPrincipal UserPrincipal principal
@@ -30,9 +38,9 @@ public class AuthExchangeController {
     }
 
     // principal이 null이면 jwt 필터가 컨텍스트를 못 세팅한 케이스 → 401
-    if (principal == null)
+    if (principal == null) {
       return ResponseEntity.status(401).build();
-
+    }
     var user = userRepository.findById(principal.userId()).orElseThrow();
 
     long accessTtl = jwtService.getRemainingTtlSeconds(accessCookie);
--- back/src/main/java/com/qmate/api/AuthMeController.java
+++ back/src/main/java/com/qmate/api/AuthMeController.java
@@ -1,6 +1,9 @@
 package com.qmate.api;
 
+import com.qmate.common.constants.auth.AuthConstants;
 import com.qmate.security.UserPrincipal;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Auth", description = "인증 관련 API")
 public class AuthMeController {
 
   public record MeResponse(Long userId, String email, String role) {}
 
   @GetMapping("/me")//프로필만
+  @Operation(
+      summary = "프로필만 조회"
+  )
   public MeResponse me(@AuthenticationPrincipal UserPrincipal p) {
     return new MeResponse(p.userId(), p.email(), p.role());
   }
