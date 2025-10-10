ac3f24d4ce35e819e6d3323d4984e7427b0ee24d
yuhyeon9808 <bwj9808@gmail.com>
2025-10-10 08:45:54 +0000

    fix api붙여서 주소 수정

--- front/src/api/axiosInstance.ts
+++ front/src/api/axiosInstance.ts
@@ -16,7 +16,7 @@ instance.interceptors.request.use(
 
     // 로그인 및 회원가입 요청에는 Authorization 헤더를 추가하지 않음
     const isAuthRoute =
-      requestUrl.startsWith('/auth/login') || requestUrl.startsWith('/auth/register');
+      requestUrl.startsWith('/api/auth/login') || requestUrl.startsWith('/api/auth/register');
 
     if (token && !isAuthRoute) {
       config.headers.Authorization = `Bearer ${token}`;
@@ -36,8 +36,7 @@ instance.interceptors.response.use(
 
     // 로그인 및 회원가입 요청에서 발생한 401은 무시
     const isAuthRoute =
-      requestUrl.startsWith('/auth/login') || requestUrl.startsWith('/auth/register');
-
+      requestUrl.startsWith('/api/auth/login') || requestUrl.startsWith('/api/auth/register');
     if (status === 401 && !isAuthRoute) {
       handleUnauthorized();
     }
